59a248af9c50edb00a6d313271fa14d165966e2a
Elad Ben-Israel <benisrae@amazon.com>
2021-05-03 11:21:27 +0300

    chore: enable projen automatic upgrades
    Add a GitHub personal access token to enable automatic upgrades of projen. The token is for the `aws-cdk-automation` user and stored under the `CDK_AUTOMATION_GITHUB_TOKEN` secret.

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,7 @@ const { web } = require('projen');
 const project = new web.ReactTypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'construct-hub-webapp',
+  projenUpgradeSecret: 'CDK_AUTOMATION_GITHUB_TOKEN',
 });
 
 project.synth();
