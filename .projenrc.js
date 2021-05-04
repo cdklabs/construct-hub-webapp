954e9849e85b90db1847db035b57e16098883156
Elad Ben-Israel <benisrae@amazon.com>
2021-05-04 10:04:24 +0300

    chore: set up packaging and release
    Set up the project so it will produce an npm tarball that contains the production static website (under `build/`), as well as a release to npm workflow.
    
    Related to #2

--- .projenrc.js
+++ .projenrc.js
@@ -4,10 +4,18 @@ const project = new web.ReactTypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'construct-hub-webapp',
   projenUpgradeSecret: 'CDK_AUTOMATION_GITHUB_TOKEN',
+  releaseToNpm: true,
+  releaseWorkflow: true,
+  package: true,
 });
 
 // synthesize project files before build
 // see https://github.com/projen/projen/issues/754
 project.tasks.tryFind('build').prependExec('npx projen');
+
+// npm tarball will only include the contents of the "build" 
+// directory, which is the output of our static website.
+project.npmignore?.addPatterns('!/build');
+project.npmignore?.addPatterns('/public');
 
 project.synth();
