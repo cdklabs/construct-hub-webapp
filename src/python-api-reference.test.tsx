7a78e34c97f942db79c0baf66865ee3b7b87352c
Eli Polonsky <epolon@amazon.com>
2021-05-21 22:18:18 +0300

    mid work

--- src/python-api-reference.test.tsx
+++ src/python-api-reference.test.tsx
@@ -14,6 +14,7 @@ export interface AssemblyFetcher {
 }
 
 export interface MarkdownHeaderOptions {
+  readonly size?: number;
   readonly title?: string;
   readonly code?: boolean;
   readonly deprecated?: boolean;
