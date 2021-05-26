eeb250ff34e014b7ea04aed143b181f2d24f0654
Mitchell Valine <mitchellvaline@gmail.com>
2021-05-25 20:59:30 -0700

    consolidate storybook in projenrc

--- .projenrc.js
+++ .projenrc.js
@@ -25,17 +25,44 @@ const project = new web.ReactTypeScriptProject({
 
   deps: ["react-router-dom"],
 
-  devDeps: [
-    "@types/react-router-dom",
+  devDeps: ["@types/react-router-dom"],
+});
+
+function addStorybook() {
+  project.addDevDeps(
     "@storybook/addon-actions",
     "@storybook/addon-essentials",
     "@storybook/addon-links",
     "@storybook/node-logger",
     "@storybook/preset-create-react-app",
     "@storybook/react",
-    "babel-loader@8.1.0",
-  ],
-});
+    "babel-loader@8.1.0"
+  );
+
+  // Add tasks and config for storybook
+  project.addTask("storybook", {
+    exec: "start-storybook -p 6006 -s public",
+  });
+
+  project.addTask("build-storybook", {
+    exec: "build-storybook -s public",
+  });
+
+  project.eslint.addOverride({
+    files: ["**/*.stories.*"],
+    rules: {
+      "import/no-anonymous-default-export": "off",
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/*.stories.*"],
+          optionalDependencies: false,
+          peerDependencies: true,
+        },
+      ],
+    },
+  });
+}
 
 // synthesize project files before build
 // see https://github.com/projen/projen/issues/754
@@ -53,32 +80,12 @@ project.eslint.addRules({
   "import/no-extraneous-dependencies": [
     "error",
     {
-      devDependencies: [
-        "**/setupTests.ts",
-        "**/*.test.tsx",
-        "**/*.test.ts",
-        "**/*.stories.*",
-      ],
+      devDependencies: ["**/setupTests.ts", "**/*.test.tsx", "**/*.test.ts"],
       optionalDependencies: false,
       peerDependencies: true,
     },
   ],
 });
-
-// Add tasks and config for storybook
-project.addTask("storybook", {
-  exec: "start-storybook -p 6006 -s public",
-});
-
-project.addTask("build-storybook", {
-  exec: "build-storybook -s public",
-});
-
-project.eslint.addOverride({
-  files: ["**/*.stories.*"],
-  rules: {
-    "import/no-anonymous-default-export": "off",
-  },
-});
+addStorybook();
 
 project.synth();
