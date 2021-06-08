648d88a9d72a3dea518660e9486d4c208a436d48
Gabe Womble <gwomble27@gmail.com>
2021-06-08 14:23:56 -0500

    feat: language bar component (#85)
    This work only implements the LanguageBar component, but does not consume it in the app. To test the component, you can launch storybook with `yarn storybook`. This is a prerequisite to #45 
    
    ![Screen Shot 2021-06-08 at 1 34 13 PM](https://user-images.githubusercontent.com/8749859/121239881-63fe7180-c85f-11eb-8413-8556f718c767.png)
    ![Screen Shot 2021-06-08 at 1 34 19 PM](https://user-images.githubusercontent.com/8749859/121239882-63fe7180-c85f-11eb-823f-9dfe5045327f.png)
    ![Screen Shot 2021-06-08 at 1 39 21 PM](https://user-images.githubusercontent.com/8749859/121239886-64970800-c85f-11eb-9828-9e930f66581a.png)
    
    Disabled languages hidden:
    ![Screen Shot 2021-06-08 at 1 48 04 PM](https://user-images.githubusercontent.com/8749859/121240837-644b3c80-c860-11eb-9fa5-74f9f6bdf715.png)
    
    A11y Tests:
    ![Screen Shot 2021-06-08 at 1 48 46 PM](https://user-images.githubusercontent.com/8749859/121240866-6b724a80-c860-11eb-9411-2fbed65abee3.png)

---
+++ src/components/LanguageBar/LanguageBar.test.tsx
@@ -0,0 +1,57 @@
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import { Language, LanguageBar, LanguageBarProps } from "./index";
+
+describe("<LanguageBar />", () => {
+  afterEach(cleanup);
+
+  function renderLanguageBar(props: Partial<LanguageBarProps>) {
+    return render(
+      <LanguageBar
+        selectedLanguage="js"
+        setSelectedLanguage={() => {}}
+        targetLanguages={["js", "python"]}
+        {...props}
+      />
+    );
+  }
+
+  test("showDisabled:false hides disabled languages", () => {
+    const targetLanguages: Language[] = ["js", "python", "golang"];
+
+    const { getByTestId, queryByTestId } = renderLanguageBar({
+      targetLanguages,
+      showDisabled: false,
+    });
+
+    targetLanguages.forEach((language) => {
+      expect(getByTestId(`language-${language}`)).not.toBeNull();
+    });
+
+    expect(queryByTestId("language-ts")).toBeNull();
+  });
+
+  test("showDisabled:true shows disabled languages", () => {
+    const { getByTestId } = renderLanguageBar({
+      targetLanguages: ["js"],
+      showDisabled: true,
+    });
+
+    const disabledElement = getByTestId("language-ts");
+    expect(disabledElement).not.toBeNull();
+    expect(disabledElement).toHaveAttribute("disabled");
+  });
+
+  test("selecting an element", () => {
+    const selectElement = jest.fn();
+
+    const { getByTestId } = renderLanguageBar({
+      targetLanguages: ["js", "dotnet"],
+      selectedLanguage: "js",
+      setSelectedLanguage: selectElement,
+    });
+
+    fireEvent.click(getByTestId("language-dotnet"));
+
+    expect(selectElement).toHaveBeenCalledWith("dotnet");
+  });
+});
