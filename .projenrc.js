77c0dba427294cbb177bb293c3811042afd2775c
iliapolo <epolon@amazon.com>
2021-06-23 23:24:27 +0300

    todo proxy

--- .projenrc.js
+++ .projenrc.js
@@ -145,10 +145,7 @@ fetchAssemblies.exec(`node scripts/fetch-assemblies.js`);
 project.gitignore.exclude("public/packages");
 
 // Proxy requests to awscdk.io for local testing
-project.package.addField(
-  "proxy",
-  "https://construct-hub-testing.dev-tools.aws.dev/"
-);
+project.package.addField("proxy", "TODO");
 
 // setup linting for create-react-app specific tools
 project.eslint.addRules({
