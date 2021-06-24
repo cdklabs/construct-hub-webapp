441759801f87dcdd8e808b1147bcd6fc116df2f2
iliapolo <epolon@amazon.com>
2021-06-24 14:55:53 +0300

    redirect to latest

---
+++ src/views/PackageLatest/PackageLatest.test.tsx
@@ -0,0 +1,82 @@
+import { Packages } from "../../api/package/packages";
+import { buildRedirectUrl } from "./index";
+
+test("redirect to latest", () => {
+  const catalog: Packages = {
+    packages: [
+      {
+        author: {
+          name: "authorName",
+          url: "authorUrl",
+        },
+        description: "packageDescription",
+        keywords: [],
+        metadata: {
+          date: "publish date",
+        },
+        name: "my-package",
+        version: "1.109.0",
+      },
+    ],
+  };
+
+  const url = buildRedirectUrl(catalog, "my-package");
+  expect(url).toEqual("/packages/my-package/v/1.109.0");
+});
+
+test("throws if missing package", () => {
+  const catalog: Packages = {
+    packages: [
+      {
+        author: {
+          name: "authorName",
+          url: "authorUrl",
+        },
+        description: "packageDescription",
+        keywords: [],
+        metadata: {
+          date: "publish date",
+        },
+        name: "my-package",
+        version: "1.109.0",
+      },
+    ],
+  };
+
+  expect(() => buildRedirectUrl(catalog, "missing-package")).toThrow();
+});
+
+test("throws if multiple packages", () => {
+  const catalog: Packages = {
+    packages: [
+      {
+        author: {
+          name: "authorName",
+          url: "authorUrl",
+        },
+        description: "packageDescription",
+        keywords: [],
+        metadata: {
+          date: "publish date",
+        },
+        name: "my-package",
+        version: "1.109.0",
+      },
+      {
+        author: {
+          name: "authorName",
+          url: "authorUrl",
+        },
+        description: "packageDescription",
+        keywords: [],
+        metadata: {
+          date: "publish date",
+        },
+        name: "my-package",
+        version: "1.109.0",
+      },
+    ],
+  };
+
+  expect(() => buildRedirectUrl(catalog, "my-package")).toThrow();
+});
