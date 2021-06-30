89120a8092abb5def629b2de2a37a6c61fe118aa
Gabe Womble <gwomble27@gmail.com>
2021-06-30 13:32:33 -0500

    added registry link support

--- src/components/OperatorArea/OperatorArea.tsx
+++ src/components/OperatorArea/OperatorArea.tsx
@@ -20,29 +20,37 @@ export const OperatorArea: FunctionComponent<OperatorAreaProps> = ({
   const details: ReactNode[] = useMemo(() => {
     if (!metadata) return [];
 
-    const { date } = metadata;
+    const { date, links } = metadata;
     const username = assembly?.author.name;
     const repository = assembly?.repository.url;
     const license = assembly?.license;
+    const registry = links?.npm;
 
     const items = [];
 
     if (date) {
-      const publishDate = <Time date={new Date(date)} format="MMMM dd, yyyy" />;
+      const publishDate = (
+        <Time date={new Date(date)} fontWeight="bold" format="MMMM dd, yyyy" />
+      );
       items.push(<>Published: {publishDate}</>);
     }
 
     if (username) {
-      items.push(`Module managed by: ${username}`);
+      const managedBy = (
+        <Box as="strong" fontWeight="bold">
+          {username}
+        </Box>
+      );
+      items.push(<>Module managed by: {managedBy}</>);
     }
 
     if (repository) {
-      const { hostname } = new URL(repository);
-      items.push(
-        <>
-          Source code: <ExternalLink href={repository}>{hostname}</ExternalLink>
-        </>
+      const repoLink = (
+        <ExternalLink href={repository}>
+          {new URL(repository).hostname}
+        </ExternalLink>
       );
+      items.push(<>Source code: {repoLink}</>);
     }
 
     if (license && license in LICENSE_LINKS) {
@@ -52,9 +60,18 @@ export const OperatorArea: FunctionComponent<OperatorAreaProps> = ({
       items.push(<>License: {licenseLink}</>);
     }
 
+    if (registry) {
+      const registryLink = (
+        <ExternalLink href={registry}>
+          {new URL(registry).hostname}
+        </ExternalLink>
+      );
+      items.push(<>Registry: {registryLink}</>);
+    }
+
     return items.map((detail: string | ReactNode, i: number) => (
       <ListItem key={`detail-${i}`} listStyleType="none" my={1}>
-        <Text color="gray.500">{detail}</Text>
+        <Text color="gray.600">{detail}</Text>
       </ListItem>
     ));
   }, [metadata, assembly]);
