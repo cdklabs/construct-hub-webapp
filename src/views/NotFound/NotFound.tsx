6232fad9405aa44fdbea47dddf3762eb849e3ba8
Gabe Womble <gwomble27@gmail.com>
2021-07-08 13:39:35 -0500

    feat: 404 page (#178)
    Adds a 404 page design!
    
    ![Screen Shot 2021-07-08 at 10 43 16 AM](https://user-images.githubusercontent.com/8749859/124951937-6cbaa280-dfd9-11eb-9b59-f4fbd526c2d5.png)

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -1,5 +1,38 @@
+import { Button, Center, Flex } from "@chakra-ui/react";
 import type { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import { Picture } from "../../components/Picture";
+
+const bgSources = {
+  optimal: [
+    { media: "(max-width:1024px)", srcSet: "assets/404-hive@50.webp" },
+    { media: "(min-width:1024px)", srcSet: "assets/404-hive@100.webp" },
+    { media: "(min-width:1024px)", srcSet: "assets/404-hive@100.png" },
+  ],
+  fallback: "assets/404-hive@50.png",
+};
 
 export const NotFound: FunctionComponent = () => {
-  return <div>Not Found</div>;
+  return (
+    <Center h="100%" position="relative" w="100%">
+      <Picture
+        alt=""
+        inset={0}
+        position="absolute"
+        sources={bgSources.optimal}
+        src={bgSources.fallback}
+        zIndex="hide"
+      />
+      <Flex align="center" direction="column" justify="center">
+        <Picture
+          alt="404 Image"
+          sources={[{ srcSet: "assets/robot.webp" }]}
+          src="assets/robot.png"
+        />
+        <Button as={Link} colorScheme="blue" to="/">
+          Take Me Home
+        </Button>
+      </Flex>
+    </Center>
+  );
 };
