206585bc9cade96dcce914ca678849f367d7545e
Gabe Womble <gwomble27@gmail.com>
2021-07-13 13:07:22 -0500

    Revert "feat: hide dependencies for now (#188)"
    This reverts commit 5a8f09311e95e2221288afd9d162f22da6402600.

--- src/views/Package/components/OperatorArea/OperatorArea.tsx
+++ src/views/Package/components/OperatorArea/OperatorArea.tsx
@@ -5,8 +5,7 @@ import type { Metadata } from "../../../../api/package/metadata";
 import { ExternalLink } from "../../../../components/ExternalLink";
 import { LicenseLink, LICENSE_LINKS } from "../../../../components/LicenseLink";
 import { Time } from "../../../../components/Time";
-// FIXME: We are temporarily hiding dependencies as they are not all present in s3
-// import { DependencyDropdown } from "../DependencyDropdown";
+import { DependencyDropdown } from "../DependencyDropdown";
 
 export interface OperatorAreaProps {
   assembly?: Assembly;
@@ -79,11 +78,11 @@ export const OperatorArea: FunctionComponent<OperatorAreaProps> = ({
   return (
     <Flex direction="column" textAlign={["center", null, "initial"]}>
       {details.length && <UnorderedList ml={0}>{details}</UnorderedList>}
-      {/* {assembly?.dependencies && (
+      {assembly?.dependencies && (
         <Box mt={4}>
           <DependencyDropdown dependencies={assembly.dependencies} />
         </Box>
-      )} */}
+      )}
     </Flex>
   );
 };
