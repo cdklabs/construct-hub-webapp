524b360bcb8b6f2091478036de6ffc126ab3d5ad
👨🏻‍💻 Romain Marcadier <rmuller@amazon.com>
2021-07-19 14:51:50 +0200

    feat: better configure ReactMarkdown
    Enable remark-gfm so that all GitHub-Flag Markdown is supported,
    including tables (Fixes cdklabs/construct-hub#197).
    
    Properly sanitize all generated markdown using `rehype-sanitize`,
    ensuring we do not render anything that GitHub would not.
    
    Transform image and link URLs that are repository-relative, so that such
    links are valid. These resolve to GitHub distribution points in the same
    way that npmjs.com does, so this change is neutral for packages that are
    not configured with a GitHub repository (Fixes cdklabs/construct-hub#198
    cdklabs/construct-hub#199).

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -1,7 +1,10 @@
 import { Box } from "@chakra-ui/react";
+import { Assembly } from "@jsii/spec";
 import { FunctionComponent } from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
+import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
 import { Code } from "./Code";
 import { Headings } from "./Headings";
 import { Hr } from "./Hr";
@@ -28,14 +31,75 @@ const components = {
   ul: Ul,
 };
 
-const rehypePlugins = [rehypeRaw];
+// Note - the default schema for rehypeSanitize is GitHub-style, which is what we need!
+const rehypePlugins = [
+  rehypeRaw,
+  // ALWAYS keep rehypeSanitize LAST!
+  rehypeSanitize,
+];
+const remarkPlugins = [remarkGfm];
 
-export const Markdown: FunctionComponent<{ children: string }> = ({
-  children,
-}) => {
+const GITHUB_REPO_REGEX =
+  /^(?:(?:git@)?github\.com:|(?:https?:\/\/)github\.com\/)([^/]+)\/([^/]+)(?:\.git)?$/;
+
+/**
+ * Parses out a GitHub repository owner and repo name from the `repository`
+ * configuration of a jsii Assembly.
+ *
+ * @returns the `owner` and `repo` for the configured repository, if it looks
+ *          like a GitHub repository URL.
+ */
+const parseGitHubRepository = ({ type, url }: Assembly["repository"]) => {
+  if (type !== "git") {
+    return undefined;
+  }
+  // git@github.com:<owner>/<repo>.git
+  // https://github.com/<owner>/<repo>.git
+  const match = GITHUB_REPO_REGEX.exec(url);
+  if (match == null) {
+    return undefined;
+  }
+
+  const [, owner, repo] = match;
+  return { owner, repo };
+};
+
+export const Markdown: FunctionComponent<{
+  children: string;
+  repository: Assembly["repository"];
+}> = ({ children, repository }) => {
+  const repoConfig = parseGitHubRepository(repository);
+
+  const toAbsoluteUri = (githubPrefix: string, githubSuffix = "HEAD") =>
+    repoConfig == null
+      ? ReactMarkdown.uriTransformer
+      : (uri: string) => {
+          const url = ReactMarkdown.uriTransformer(uri);
+
+          // If this is an anchor or absolute URL, return it.
+          const [first] = url;
+          if (first === "#" || first === "/") {
+            return url;
+          }
+
+          // If there is a protocol element, then return the URL as-is.
+          if (url.includes("://")) {
+            return url;
+          }
+
+          const { owner, repo } = repoConfig;
+          return `https://${githubPrefix}/${owner}/${repo}/${githubSuffix}/${url}`;
+        };
+
   return (
     <Box sx={{ "& > *": { mb: 4 } }}>
-      <ReactMarkdown components={components} rehypePlugins={rehypePlugins}>
+      <ReactMarkdown
+        components={components}
+        rehypePlugins={rehypePlugins}
+        remarkPlugins={remarkPlugins}
+        transformImageUri={toAbsoluteUri("raw.githubusercontent.com")}
+        transformLinkUri={toAbsoluteUri("github.com", "blob/HEAD")}
+      >
         {children}
       </ReactMarkdown>
     </Box>
