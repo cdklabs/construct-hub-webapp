23980803e79011909bdf3d92396eafabc9bd1a46
Gabe Womble <gwomble27@gmail.com>
2021-07-20 15:03:13 -0500

    feat: faq-page (lorem text)

---
+++ src/views/FAQ/FAQSection.tsx
@@ -0,0 +1,36 @@
+import {
+  AccordionIcon,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Heading,
+  Box,
+  Accordion,
+} from "@chakra-ui/react";
+
+import type { FunctionComponent } from "react";
+
+export interface FAQSectionProps {
+  heading: string;
+}
+
+export const FAQSection: FunctionComponent<FAQSectionProps> = ({
+  heading,
+  children,
+}) => (
+  <AccordionItem>
+    <AccordionButton>
+      <Box flex="1" textAlign="left">
+        <Heading as="h2" ml={4} size="lg">
+          {heading}
+        </Heading>
+      </Box>
+      <AccordionIcon />
+    </AccordionButton>
+    <AccordionPanel p={0}>
+      <Accordion allowMultiple w="100%">
+        {children}
+      </Accordion>
+    </AccordionPanel>
+  </AccordionItem>
+);
