70e50d2022a0a084e947786ffc0173ac0d714823
Eli Polonsky <Eli.polonsky@gmail.com>
2021-07-26 17:03:39 +0300

    fix: missing doc pages result in an unclear error  (#260)
    Currently, when users browse to a documentation page in a language that is either:
    
    1. Unsupported by the package
    2. Documentation isn't ready for yet
    3. Documentation failed to create for
    
    This is what we show:
    
    ![Screen Shot 2021-07-25 at 11 36 11 AM](https://user-images.githubusercontent.com/1428812/126893012-8e23d701-be4d-4c8d-97de-0d3d56605f92.png)
    
    > Example: https://constructs.dev/packages/@softchef/cdk-vue/v/1.0.31?lang=python
    
    This happens because we currently don't detect that we requested a missing document, and we simply render the `index.html` page that CloudFront serves in such scenarios. 
    
    This PR adds this missing docs page detection by asserting that the content type of the response is the one we expect. 
    It works because our doc pages have `text/markdown`, while our `index.html` has `text/html`. 
    
    This is just a tactical stop-gap until we implement proper 404 responses for such cases on the backend. 
    
    Following this PR, these errors will displayed as:
    
    ![Screen Shot 2021-07-25 at 11 26 42 AM](https://user-images.githubusercontent.com/1428812/126893094-60985395-b3f1-4f7b-bf1c-a00095eb99a3.png)
    
    The error still needs some design, but at least it's clear and coherent. 
    
    Fixes https://github.com/cdklabs/construct-hub-webapp/issues/255
    Fixes https://github.com/cdklabs/construct-hub/issues/218

---
+++ src/views/Package/components/PackageDocsError/PackageDocsError.tsx
@@ -0,0 +1,30 @@
+import { Text } from "@chakra-ui/react";
+import { FunctionComponent } from "react";
+import { ExternalLink } from "../../../../components/ExternalLink";
+import { Language } from "../../../../constants/languages";
+
+export interface PackageDocsErrorProps {
+  readonly language: Language;
+}
+
+export const PackageDocsError: FunctionComponent<PackageDocsErrorProps> = ({
+  language,
+}) => {
+  const issueLink = (
+    <ExternalLink href="https://github.com/cdklabs/construct-hub-webapp/issues/new">
+      issue
+    </ExternalLink>
+  );
+  return (
+    <Text
+      align="center"
+      fontSize="xl"
+      fontStyle="oblique"
+      wordBreak="break-word"
+    >
+      Documentation in {language} is still not ready for this package. Come back
+      soon. If this issue persists, please let us know by creting an {issueLink}
+      .
+    </Text>
+  );
+};
