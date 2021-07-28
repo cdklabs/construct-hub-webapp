973bd0772cb1d8523a512ac3338e71d0ce983f7a
Gabe Womble <gwomble27@gmail.com>
2021-07-28 16:26:44 -0500

    better

--- src/components/Markdown/WindowedMarkdown.tsx
+++ src/components/Markdown/WindowedMarkdown.tsx
@@ -3,24 +3,19 @@
  * that attempts to implement windowing via intersection observers and chunking the README
  */
 import { Box } from "@chakra-ui/react";
-import { FunctionComponent, useLayoutEffect, useRef, useState } from "react";
+import { memo, useLayoutEffect, useMemo, useRef, useState } from "react";
 import ReactMarkdown, { ReactMarkdownOptions } from "react-markdown";
 
-export interface ExpMarkdownProps extends ReactMarkdownOptions {
+export interface WindowedMarkdownProps extends ReactMarkdownOptions {
   children: string;
 }
 
-export interface ChunkProps extends ExpMarkdownProps {
+export interface ChunkProps extends WindowedMarkdownProps {
   isReady: boolean;
   id: number;
 }
 
-const Chunk: FunctionComponent<ChunkProps> = ({
-  children,
-  isReady,
-  id,
-  ...props
-}) => {
+const Chunk = memo<ChunkProps>(({ children, isReady, id, ...props }) => {
   return (
     <Box data-chunk data-id={id}>
       {isReady ? (
@@ -32,60 +27,68 @@ const Chunk: FunctionComponent<ChunkProps> = ({
       )}
     </Box>
   );
-};
+});
+
+Chunk.displayName = "Chunk";
 
 const separator = `
 ---
 `;
 
-export const WindowedMarkdown: FunctionComponent<ExpMarkdownProps> = ({
-  children,
-  ...props
-}) => {
-  const root = useRef<HTMLDivElement>(null);
-  const [visibilityMap, setVisibilityMap] = useState<Record<string, boolean>>(
-    {}
-  );
-  const chunks = children.split(separator);
+export const WindowedMarkdown = memo<WindowedMarkdownProps>(
+  ({ children, ...props }) => {
+    const root = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
-    const rootEl = root.current ?? document; // Redundancy to be safe
+    const [visibilityMap, setVisibilityMap] = useState<Record<string, boolean>>(
+      {}
+    );
 
-    // Instantiate IntersectionObserver
-    const observer = new IntersectionObserver((entries, obs) => {
-      let updatesToMake: Record<string, boolean> = {};
+    const chunks = useMemo(() => children.split(separator), [children]);
 
-      entries.forEach((entry) => {
-        const id = entry.target.getAttribute("data-id") ?? "";
-        if (entry.isIntersecting && !visibilityMap[id]) {
-          updatesToMake[id] = true;
-          obs.unobserve(entry.target);
+    useLayoutEffect(() => {
+      const rootEl = root.current ?? document; // Redundancy to be safe
+
+      // Instantiate IntersectionObserver
+      const observer = new IntersectionObserver((entries, obs) => {
+        let updatesToMake: Record<string, boolean> = {};
+
+        entries.forEach((entry) => {
+          const id = entry.target.getAttribute("data-id") ?? "";
+          if (entry.isIntersecting && !visibilityMap[id]) {
+            updatesToMake[id] = true;
+            obs.unobserve(entry.target);
+          }
+        });
+
+        if (Object.keys(updatesToMake).length) {
+          window.requestAnimationFrame(() => {
+            setVisibilityMap((current) => ({ ...current, ...updatesToMake }));
+          });
         }
       });
 
-      if (Object.keys(updatesToMake).length) {
-        window.requestAnimationFrame(() => {
-          setVisibilityMap((current) => ({ ...current, ...updatesToMake }));
-        });
-      }
-    });
+      // Observe the targets
+      rootEl.querySelectorAll("[data-chunk]").forEach((chunk) => {
+        observer.observe(chunk);
+      });
 
-    // Observe the targets
-    rootEl.querySelectorAll("[data-chunk]").forEach((chunk) => {
-      observer.observe(chunk);
-    });
+      return () => observer.disconnect();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [children]);
 
-    return () => observer.disconnect();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [children]);
-
-  return (
-    <Box ref={root}>
-      {chunks.map((chunk, i) => (
-        <Chunk id={i} isReady={i === 0 || visibilityMap[i]} key={i} {...props}>
-          {`${chunk}${i < chunks.length - 1 ? separator : ""}`}
-        </Chunk>
-      ))}
-    </Box>
-  );
-};
+    return (
+      <Box ref={root}>
+        {chunks.map((chunk, i) => (
+          <Chunk
+            id={i}
+            isReady={i === 0 || visibilityMap[i]}
+            key={i}
+            {...props}
+          >
+            {`${chunk}${i < chunks.length - 1 ? separator : ""}`}
+          </Chunk>
+        ))}
+      </Box>
+    );
+  }
+);
