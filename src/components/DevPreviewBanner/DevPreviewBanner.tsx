fdd32facfce224f8c0f6c7214a7cb9cc07c20e74
iliapolo <epolon@amazon.com>
2021-07-29 15:48:08 +0300

    banner cannot be dismissed now

--- src/components/DevPreviewBanner/DevPreviewBanner.tsx
+++ src/components/DevPreviewBanner/DevPreviewBanner.tsx
@@ -1,28 +1,13 @@
-import { CloseIcon, InfoOutlineIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  IconButton,
-  Collapse,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
+import { Box, Collapse, Text } from "@chakra-ui/react";
 import type { FunctionComponent } from "react";
 import { Card } from "../Card";
 import { ExternalLink } from "../ExternalLink";
 
-const STORAGE_KEY = "showing-dev-preview-banner";
-
 export const DevPreviewBanner: FunctionComponent = () => {
-  const { isOpen, onClose } = useDisclosure({
-    defaultIsOpen: JSON.parse(
-      window.sessionStorage.getItem(STORAGE_KEY) ?? "true"
-    ),
-    onClose: () => window.sessionStorage.setItem(STORAGE_KEY, "false"),
-  });
-
   return (
     <Box h="max-content">
-      <Collapse in={isOpen}>
+      <Collapse in={true}>
         <Card
           aria-label="Preview Banner"
           bg="blue.500"
@@ -48,17 +33,6 @@ export const DevPreviewBanner: FunctionComponent = () => {
               here
             </ExternalLink>
           </Text>
-          <IconButton
-            aria-label="Dismiss banner"
-            colorScheme="white"
-            icon={<CloseIcon />}
-            onClick={onClose}
-            position="absolute"
-            right={4}
-            size="xs"
-            top={3}
-            variant="ghost"
-          />
         </Card>
       </Collapse>
     </Box>
