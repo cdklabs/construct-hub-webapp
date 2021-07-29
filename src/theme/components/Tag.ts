7ecabd3d4fbb2b927c53ce3b5f35f79a0c5b8b7a
Mitchell Valine <mitchellvaline@yahoo.com>
2021-07-29 12:25:01 -0700

    fix: search result card styling
    Updates the spacing of items within the search results card and
    customizes the Tag component to better match design.

---
+++ src/theme/components/Tag.ts
@@ -0,0 +1,42 @@
+import { theme } from "@chakra-ui/react";
+
+const createVariant =
+  (
+    variant: (props: Record<string, any>) => {
+      container: { [key: string]: any };
+    },
+    overrides: Record<string, any>
+  ) =>
+  (props: Record<string, any>) => {
+    const varBase = variant(props);
+    return {
+      ...varBase,
+      container: {
+        ...varBase.container,
+        ...overrides,
+      },
+    };
+  };
+
+const {
+  components: { Tag: base },
+} = theme;
+
+export const Tag = {
+  ...base,
+  baseStyle: {
+    container: {
+      fontWeight: "normal",
+    },
+  },
+  variants: {
+    ...base.variants,
+    subtle: createVariant(base.variants.subtle, {
+      background: "#F2F2F2",
+    }),
+    official: createVariant(base.variants.subtle, {
+      background: "rgba(33, 150, 83, 0.1)",
+      color: "#219653",
+    }),
+  },
+};
