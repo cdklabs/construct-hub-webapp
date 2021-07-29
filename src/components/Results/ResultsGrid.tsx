54bda9d28c3fd9a280d8997e98ffbbd37a176a3b
Mitchell Valine <mitchellvaline@yahoo.com>
2021-07-29 12:55:41 -0700

    fix: search result card styling (#294)
    Updates the spacing of items within the search results card and
    customizes the Tag component to better match design.

--- src/components/Results/ResultsGrid.tsx
+++ src/components/Results/ResultsGrid.tsx
@@ -2,7 +2,17 @@ import { SimpleGrid } from "@chakra-ui/react";
 import type { FunctionComponent } from "react";
 
 export const ResultsGrid: FunctionComponent = ({ children }) => (
-  <SimpleGrid columns={{ base: 1, md: 3, xl: 5 }} spacing={6}>
+  <SimpleGrid
+    columns={{
+      base: 1,
+      sm: 2,
+      md: 3,
+      lg: 4,
+      xl: 5,
+      "2xl": 6,
+    }}
+    spacing={6}
+  >
     {children}
   </SimpleGrid>
 );
--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,9 +1,11 @@
 import { theme } from "@chakra-ui/react";
 import { Code } from "./Code";
 import { Divider } from "./Divider";
+import { Tag } from "./Tag";
 
 export const components = {
   ...theme.components,
   Code,
   Divider,
+  Tag,
 };
