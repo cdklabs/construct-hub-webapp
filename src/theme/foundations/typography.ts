06f70031c34b879c2abb53d837c968db23deb558
iliapolo <epolon@amazon.com>
2021-07-30 16:39:58 +0300

    remove heebo font

--- src/theme/foundations/typography.ts
+++
@@ -1,7 +0,0 @@
-export const typography = {
-  fonts: {
-    heading: `"Heebo", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-    body: `"Heebo", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-    mono: `SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace`,
-  },
-};
