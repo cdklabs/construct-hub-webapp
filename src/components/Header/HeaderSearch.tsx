8758c1437d26fdce96417da7e0ec2bb7625093a3
Gabe Womble <gwomble27@gmail.com>
2021-10-08 15:15:48 -0700

    Use Switch + Route combo for url based search rendering

--- src/components/Header/HeaderSearch.tsx
+++ src/components/Header/HeaderSearch.tsx
@@ -1,7 +1,7 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import { Box, IconButton, useDisclosure } from "@chakra-ui/react";
 import type { FunctionComponent } from "react";
-import { useLocation } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { ROUTES } from "../../constants/url";
 import { SearchBar, SearchOverlay, SearchSuggestions } from "../SearchBar";
 import { SearchModal } from "../SearchModal";
@@ -12,37 +12,35 @@ import testIds from "./testIds";
  */
 export const HeaderSearch: FunctionComponent = () => {
   const searchModal = useDisclosure();
-  const { pathname } = useLocation();
-
-  if ([ROUTES.HOME, ROUTES.SEARCH].some((path) => path === pathname)) {
-    return null;
-  }
 
   return (
-    <>
-      {/* Desktop / Tablet Search Trigger */}
-      <Box
-        data-testid={testIds.searchInput}
-        display={{ base: "none", md: "initial" }}
-      >
-        <SearchBar bg="gray.50">
-          <SearchOverlay />
-          <SearchSuggestions />
-        </SearchBar>
-      </Box>
-      {/* Mobile Search Trigger */}
-      <Box display={{ base: "initial", md: "none" }}>
-        <IconButton
-          aria-label="Search Icon"
-          borderRadius="md"
-          data-testid={testIds.searchIcon}
-          icon={<SearchIcon color="gray.600" />}
-          onClick={searchModal.onOpen}
-          variant="ghost"
-        />
+    <Switch>
+      <Route exact path={[ROUTES.HOME, ROUTES.SEARCH]} />
+      <Route path="*">
+        {/* Desktop / Tablet Search Trigger */}
+        <Box
+          data-testid={testIds.searchInput}
+          display={{ base: "none", md: "initial" }}
+        >
+          <SearchBar bg="gray.50">
+            <SearchOverlay />
+            <SearchSuggestions />
+          </SearchBar>
+        </Box>
+        {/* Mobile Search Trigger */}
+        <Box display={{ base: "initial", md: "none" }}>
+          <IconButton
+            aria-label="Search Icon"
+            borderRadius="md"
+            data-testid={testIds.searchIcon}
+            icon={<SearchIcon color="gray.600" />}
+            onClick={searchModal.onOpen}
+            variant="ghost"
+          />
 
-        <SearchModal {...searchModal} />
-      </Box>
-    </>
+          <SearchModal {...searchModal} />
+        </Box>
+      </Route>
+    </Switch>
   );
 };
