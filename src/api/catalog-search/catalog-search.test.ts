22596d705678eae3e01d4f0a312a629af179c879
Christopher Rybicki <rybickic@amazon.com>
2021-10-19 13:23:34 -0700

    fix tests, update caching

--- src/api/catalog-search/catalog-search.test.ts
+++ src/api/catalog-search/catalog-search.test.ts
@@ -1,14 +1,17 @@
 import catalogFixture from "../../__fixtures__/catalog.json";
+import statsFixture from "../../__fixtures__/stats.json";
 import { CDKType } from "../../constants/constructs";
 import { Language } from "../../constants/languages";
 import { CatalogPackage } from "../package/packages";
+import { PackageStats } from "../stats";
 import { CatalogSearchAPI } from "./catalog-search";
 import { CatalogSearchSort } from "./constants";
 import * as util from "./util";
 
 describe("CatalogSearchAPI", () => {
   const instance = new CatalogSearchAPI(
-    catalogFixture.packages as CatalogPackage[]
+    catalogFixture.packages as CatalogPackage[],
+    statsFixture as PackageStats
   );
 
   it("exposes a property which returns detected cdk frameworks", () => {
