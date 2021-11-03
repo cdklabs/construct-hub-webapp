0e53e23d6f73d9b1c0bcbfc011a53649b37815ab
Gabe Womble <gwomble27@gmail.com>
2021-11-03 16:28:19 -0700

    feat: show disabled languages in filters

--- src/views/SearchRedesign/CheckboxFilter.tsx
+++ src/views/SearchRedesign/CheckboxFilter.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Stack, Text } from "@chakra-ui/react";
+import { Checkbox, Stack, Text, Tooltip } from "@chakra-ui/react";
 import type { FunctionComponent } from "react";
 import { FilterHeading, FilterHeadingProps } from "./FilterHeading";
 import testIds from "./testIds";
@@ -8,6 +8,8 @@ export interface CheckboxFilterProps extends FilterHeadingProps {
   options: {
     display: string;
     value: string;
+    isDisabled?: boolean;
+    disabledHint?: string;
   }[];
   values: string[];
   onValueChange: (value: string) => void;
@@ -28,21 +30,29 @@ export const CheckboxFilter: FunctionComponent<CheckboxFilterProps> = ({
   return (
     <Stack data-testid={dataTestid} spacing={2}>
       <FilterHeading hint={hint} name={name} />
-      {options.map(({ display, value }) => (
+      {options.map(({ disabledHint, display, isDisabled, value }) => (
         <Checkbox
           isChecked={checkedValues.includes(value)}
+          isDisabled={isDisabled}
           key={value}
           onChange={getOnChange(value)}
         >
-          <Text
-            color="gray.600"
-            data-testid={testIds.filterItem}
-            data-value={value}
-            fontSize="sm"
-            isTruncated
+          <Tooltip
+            hasArrow
+            isDisabled={!isDisabled && !disabledHint}
+            label={disabledHint}
+            placement="right"
           >
-            {display}
-          </Text>
+            <Text
+              color="gray.600"
+              data-testid={testIds.filterItem}
+              data-value={value}
+              fontSize="sm"
+              isTruncated
+            >
+              {display}
+            </Text>
+          </Tooltip>
         </Checkbox>
       ))}
     </Stack>
