77c36c8ca4041f88d8eb82aa86a5c27ea52adc24
Gabe Womble <gwomble27@gmail.com>
2021-11-04 16:11:41 -0700

    Rephrase IAC to IaC
    Co-authored-by: Christopher Rybicki <rybickic@amazon.com>

--- src/views/SearchRedesign/CDKFilter.tsx
+++ src/views/SearchRedesign/CDKFilter.tsx
@@ -78,7 +78,7 @@ export const CDKFilter: FunctionComponent = () => {
     <>
       <RadioFilter
         data-testid={testIds.cdkTypeFilter}
-        hint="Choose the right CDK for your IAC technology: AWS CDK for AWS CloudFormation, CDKtf for Terraform, or CDK8s for Kubernetes."
+        hint="Choose the right CDK for your IaC technology: AWS CDK for AWS CloudFormation, CDKtf for Terraform, or CDK8s for Kubernetes."
         name="CDK Type"
         onValueChange={onCdkTypeChange}
         options={[
