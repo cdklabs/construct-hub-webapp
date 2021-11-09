573e1fad4e4a7ec24385b8bb14365cc8b1a51f6d
Gabe Womble <gwomble27@gmail.com>
2021-11-09 10:51:36 -0800

    chore: filter ux adjustments (#626)
    Implements the following changes:
    - Ensure all spaces between filter groups = 16px
    - Ensure all spaces between filter label and filter values = 8px
    - reduce radio buttons and checkboxes to have size 14x14px
    - space between radio buttons and checkboxes = 4px
    - Modify question mark icon size from 16x16 to 14x14
    - Reduce spacing between tooltip icon and label to 6px

--- src/views/SearchRedesign/FilterHeading.tsx
+++ src/views/SearchRedesign/FilterHeading.tsx
@@ -4,7 +4,6 @@ import {
   Heading,
   Text,
   Popover,
-  IconButton,
   PopoverTrigger,
   PopoverBody,
   PopoverArrow,
@@ -22,22 +21,16 @@ export const FilterHeading: FunctionComponent<FilterHeadingProps> = ({
   hint,
 }) => {
   return (
-    <Flex align="center">
+    <Flex align="center" mb={1}>
       <Heading as="h4" size="sm" w="max-content">
         {name}
       </Heading>
       {hint ? (
         <Popover colorScheme="dark" placement="right" strategy="fixed">
           <PopoverTrigger>
-            <IconButton
-              aria-label={`Hint: ${name}`}
-              icon={<QuestionIcon h={4} w={4} />}
-              ml={2}
-              px={0}
-              py={0}
-              size="sm"
-              variant="ghost"
-            />
+            <Flex aria-label={`Hint: ${name}`} as="button" ml={2}>
+              <QuestionIcon h={3.5} w={3.5} />
+            </Flex>
           </PopoverTrigger>
           <PopoverContent
             bg="gray.700"
