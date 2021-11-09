5595cd8ebd5df68b02cd2f40077950b545fdcc1e
Gabe Womble <gwomble27@gmail.com>
2021-11-09 11:43:18 -0800

    chore: tab adjustments

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -2,17 +2,19 @@ import { theme } from "@chakra-ui/react";
 import { PackageTagConfig } from "../../api/config";
 import { Code } from "./Code";
 import { Divider } from "./Divider";
+import { Tabs } from "./Tabs";
 import { makeTag } from "./Tag";
 
 interface ComponentsConfig {
   Tag: PackageTagConfig[];
 }
 
-export const makeComponents = (config: ComponentsConfig) => {
+export const makeComponents: any = (config: ComponentsConfig) => {
   return {
     ...theme.components,
     Code,
     Divider,
     Tag: makeTag(config.Tag),
+    Tabs,
   };
 };
