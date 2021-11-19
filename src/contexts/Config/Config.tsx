1afebead53cb4f3d556117058c2beed7d657b854
Elad Ben-Israel <benisrae@amazon.com>
2021-11-19 16:09:55 +0200

    feat: minimal home page (#716)
    Remove a few sections from the home page for and introduce a feature flag `fullSite` which offers the full experience.
    
    Also, now feature flags can be enabled using query params (e.g `?ff-fullSite`).

--- src/contexts/Config/Config.tsx
+++ src/contexts/Config/Config.tsx
@@ -5,6 +5,7 @@ import {
   useEffect,
   useState,
 } from "react";
+import { useLocation } from "react-router-dom";
 import { fetchConfig, Config } from "../../api/config";
 import { useRequest, UseRequestResponse } from "../../hooks/useRequest";
 
@@ -19,6 +20,7 @@ const LOCAL_CONFIG = "construct-hub-config";
 export const useConfig = () => useContext(ConfigContext);
 
 export const ConfigProvider: FunctionComponent = ({ children }) => {
+  const { search } = useLocation();
   const [configData, setConfigData] = useState<Config | undefined>(() => {
     try {
       const stored = localStorage.getItem(LOCAL_CONFIG);
@@ -48,8 +50,24 @@ export const ConfigProvider: FunctionComponent = ({ children }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const config = configData ?? {};
+
+  // allow overriding feature flags using query params the query param name is
+  // `ff-<flagName>`, for example,
+  // `https://constructs.dev?ff-generalAvailability`.
+  const params = new URLSearchParams(search);
+  const flags: any = config.featureFlags ?? {};
+  config.featureFlags = flags;
+
+  for (const key of params.keys()) {
+    if (key.startsWith("ff-")) {
+      const flagName = key.slice(3);
+      flags[flagName] = true;
+    }
+  }
+
   return (
-    <ConfigContext.Provider value={{ ...configResponse, data: configData }}>
+    <ConfigContext.Provider value={{ ...configResponse, data: config }}>
       {children}
     </ConfigContext.Provider>
   );
