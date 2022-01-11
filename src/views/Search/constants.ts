86b7d61a51f8179cbf2be17fe217dc0030a7cca7
Gabe Womble <womblg@amazon.com>
2022-01-11 15:00:50 -0800

    chore: reword newest to recently updated

--- src/views/Search/constants.ts
+++ src/views/Search/constants.ts
@@ -10,8 +10,8 @@ export type SearchQueryParam = Extract<QueryParamKey, "offset" | "q">;
 export const SORT_RENDER_MAP = {
   [CatalogSearchSort.NameAsc]: "A-Z",
   [CatalogSearchSort.NameDesc]: "Z-A",
-  [CatalogSearchSort.PublishDateAsc]: "Oldest first",
-  [CatalogSearchSort.PublishDateDesc]: "Newest first",
+  [CatalogSearchSort.PublishDateAsc]: "Oldest",
+  [CatalogSearchSort.PublishDateDesc]: "Recently updated",
   [CatalogSearchSort.DownloadsDesc]: "Most downloads",
   [CatalogSearchSort.DownloadsAsc]: "Least downloads",
 };
