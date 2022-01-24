357bb5c795c083f02502829799725bc3fd71abc0
Gabe Womble <womblg@amazon.com>
2022-01-24 09:27:17 -0800

    footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,13 @@
 import {
-  Box,
   Button,
   ButtonProps,
-  Divider,
   Flex,
-  SimpleGrid,
   Text,
   useColorMode,
 } from "@chakra-ui/react";
 import type { FunctionComponent } from "react";
 import { useShortBread } from "../../contexts/Shortbread";
+import { SECTION_PADDING } from "../../views/Home/constants";
 import { ExternalLink } from "../ExternalLink";
 import { NavLink } from "../NavLink";
 import { DISCLAIMER, FOOTER_LINKS } from "./constants";
@@ -21,10 +19,9 @@ const LinkButton: FunctionComponent<
   ButtonProps & { "data-testid"?: string }
 > = (props) => (
   <Button
-    color="white"
-    fontSize="xs"
+    color="inherit"
+    fontSize="inherit"
     fontWeight="normal"
-    mt={4}
     variant="link"
     {...props}
   />
@@ -36,79 +33,68 @@ export const Footer: FunctionComponent<FooterProps> = () => {
 
   return (
     <Flex
-      align="center"
       as="footer"
       bg="brand.800"
       color="white"
       data-testid={testIds.container}
       direction="column"
-      justify="center"
+      fontSize="sm"
+      px={SECTION_PADDING.X}
       py={4}
     >
-      <SimpleGrid columnGap={6} columns={[1, 2, 4]} data-testid={testIds.links}>
-        {Object.entries(FOOTER_LINKS).map(
-          ([key, { display, isExternal = true, testId, url }], index) => (
-            <Flex
-              align="center"
-              direction={{ base: "column", md: "row" }}
-              key={key}
-            >
-              {/* Single Row Divider */}
-              <Box
-                display={{ base: "none", md: index !== 0 ? "initial" : "none" }}
-                h={5}
+      <Flex align="center" gap={4} justify="space-between" w="full" wrap="wrap">
+        {/* AWS Links */}
+        <Flex align="center" gap={4}>
+          {Object.entries(FOOTER_LINKS).map(
+            ([key, { display, isExternal = true, testId, url }]) => (
+              <Flex
+                align="center"
+                direction={{ base: "column", md: "row" }}
+                key={key}
               >
-                <Divider borderColor="white" mr={6} orientation="vertical" />
-              </Box>
-              {isExternal ? (
-                <ExternalLink
-                  color="currentcolor"
-                  data-testid={testIds[testId]}
-                  hasWarning={false}
-                  href={url}
-                  lineHeight={10}
-                  mx="auto"
-                >
-                  {display}
-                </ExternalLink>
-              ) : (
-                <NavLink
-                  color="currentcolor"
-                  data-testid={testIds[testId]}
-                  lineHeight={10}
-                  mx="auto"
-                  to={url}
-                >
-                  {display}
-                </NavLink>
-              )}
-              {/* 2 Row Divider */}
-              <Box
-                display={{
-                  base: "none",
-                  sm: index < 2 ? "initial" : "none",
-                  md: "none",
-                }}
-                w="100%"
-              >
-                <Divider borderColor="white" />
-              </Box>
-            </Flex>
-          )
-        )}
-      </SimpleGrid>
-      <Text data-testid={testIds.disclaimer} fontSize="xs" mt={4}>
+                {isExternal ? (
+                  <ExternalLink
+                    color="currentcolor"
+                    data-testid={testIds[testId]}
+                    hasWarning={false}
+                    href={url}
+                    mx="auto"
+                  >
+                    {display}
+                  </ExternalLink>
+                ) : (
+                  <NavLink
+                    color="currentcolor"
+                    data-testid={testIds[testId]}
+                    mx="auto"
+                    to={url}
+                  >
+                    {display}
+                  </NavLink>
+                )}
+              </Flex>
+            )
+          )}
+        </Flex>
+
+        {/* Manage Cookies / Color mode toggle */}
+        <Flex align="center" gap={4}>
+          <LinkButton
+            data-testid={testIds.manageCookies}
+            onClick={customizeCookies}
+          >
+            Manage Cookies
+          </LinkButton>
+          <LinkButton onClick={toggleColorMode}>
+            View in {colorMode === "light" ? "dark" : "light"} mode
+          </LinkButton>
+        </Flex>
+      </Flex>
+
+      {/* Disclaimer Text */}
+      <Text data-testid={testIds.disclaimer} mt={4}>
         {DISCLAIMER}
       </Text>
-      <LinkButton
-        data-testid={testIds.manageCookies}
-        onClick={customizeCookies}
-      >
-        Manage Cookies
-      </LinkButton>
-      <LinkButton onClick={toggleColorMode}>
-        View in {colorMode === "light" ? "dark" : "light"} mode
-      </LinkButton>
     </Flex>
   );
 };
